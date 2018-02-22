fe8f79938b194d1015e0cdc519d1c9104398ade6
Ariana M. Davis <hello@yourmobilegeek.tech>
2018-02-22 12:44:56 -0500

    updated component files

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: 
+  <p> This is my html </p>
+  './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
